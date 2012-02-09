452628827954f753bd70175ce54a38b2af60cae5
Brad Phelan <bradphelan@xtargets.com>
2012-02-09 17:33:02 +0100

    Add the config.less.paths to the config.asset.paths
    so that we can resolve paths using the context object

--- lib/less/rails/railtie.rb
+++ lib/less/rails/railtie.rb
@@ -30,6 +30,7 @@ module Less
       
       initializer 'less-rails.after.append_assets_path', :after => :append_assets_path, :group => :all do |app|
         assets_stylesheet_paths = app.config.assets.paths.select { |p| p.ends_with?('stylesheets') }
+        app.config.assets.paths+=app.config.less.paths
         app.config.less.paths.unshift(*assets_stylesheet_paths)
       end
       

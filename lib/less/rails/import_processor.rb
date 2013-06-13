583e8fe34714127e4de24105a4a0c59e9c717b13
Garrett Motzner <garrett@quickleft.com>
2013-06-13 12:08:06 -0600

    WIP Fix import hooks to allow relative paths to files Still has an issue where the Less Parser can't find the import file under some circumstances.

--- lib/less/rails/import_processor.rb
+++ lib/less/rails/import_processor.rb
@@ -11,17 +11,23 @@ module Less
         depend_on scope, data
         data
       end
-      
-      def depend_on(scope, data)
+
+      def depend_on(scope, data, base="")
         import_paths = data.scan(IMPORT_SCANNER).flatten.compact.uniq
         import_paths.each do |path|
+          tried_relative_path = false
           pathname = begin
                        scope.resolve(path)
                      rescue Sprockets::FileNotFound
+                       if !tried_relative_path
+                         path = File.join(base, path)
+                         tried_relative_path = true
+                         retry
+                       end
                        nil
                      end
           scope.depend_on(path) if pathname && pathname.to_s.ends_with?('.less')
-          depend_on scope, File.read(pathname) if pathname
+          depend_on scope, File.read(pathname), File.dirname(path) if File.exists?(pathname)
         end
         data
       end

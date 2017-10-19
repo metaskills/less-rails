01d79c0e0d26be3ce159453f2b12428c9dbf347d
Brendon Muir <brendon@spike.net.nz>
2017-10-19 14:18:43 +1300

    Fix whitespace

--- lib/less/rails/helpers.rb
+++ lib/less/rails/helpers.rb
@@ -1,9 +1,9 @@
 module Less
-  
+
   def self.less
     @less
   end
-  
+
   def self.register_rails_helper(name, &block)
     tree = @loader.require('less/tree')
     tree.functions[name] = lambda do |*args|
@@ -12,12 +12,12 @@ module Less
       tree[:Anonymous].new block.call(tree, args.last)
     end
   end
-  
+
   module Rails
     module Helpers
-      
+
       extend ActiveSupport::Concern
-      
+
       included do
         Less.register_rails_helper('asset-path')      { |tree, cxt| asset_path       unquote(cxt.toCSS()) }
         Less.register_rails_helper('asset-url')       { |tree, cxt| asset_url        unquote(cxt.toCSS()) }
@@ -35,21 +35,21 @@ module Less
         Less.register_rails_helper('font-url')        { |tree, cxt| font_url         unquote(cxt.toCSS()) }
         Less.register_rails_helper('asset-data-url')  { |tree, cxt| asset_data_url   unquote(cxt.toCSS()) }
       end
-      
+
       module ClassMethods
-        
+
         def asset_data_url(path)
           "url(#{scope.asset_data_uri(path)})"
         end
-        
+
         def asset_path(asset)
           public_path(asset).inspect
         end
-        
+
         def asset_url(asset)
           "url(#{public_path(asset)})"
         end
-        
+
         def image_path(img)
           scope.image_path(img).inspect
         end
@@ -61,31 +61,31 @@ module Less
         def video_path(video)
           scope.video_path(video).inspect
         end
-        
+
         def video_url(video)
           "url(#{scope.video_path(video)})"
         end
-        
+
         def audio_path(audio)
           scope.audio_path(audio).inspect
         end
-        
+
         def audio_url(audio)
           "url(#{scope.audio_path(audio)})"
         end
-        
+
         def javascript_path(javascript)
           scope.javascript_path(javascript).inspect
         end
-        
+
         def javascript_url(javascript)
           "url(#{scope.javascript_path(javascript)})"
         end
-        
+
         def stylesheet_path(stylesheet)
           scope.stylesheet_path(stylesheet).inspect
         end
-        
+
         def stylesheet_url(stylesheet)
           "url(#{scope.stylesheet_path(stylesheet)})"
         end
@@ -97,7 +97,7 @@ module Less
             asset_path(font)
           end
         end
-        
+
         def font_url(font)
           if scope.respond_to?(:font_path)
             "url(#{scope.font_path(font)})"
@@ -105,13 +105,13 @@ module Less
             asset_url(font)
           end
         end
-        
+
         private
-        
+
         def scope
           Less.Parser['scope']
         end
-        
+
         def public_path(asset)
           if scope.respond_to?(:asset_paths)
             scope.asset_paths.compute_public_path asset, ::Rails.application.config.assets.prefix
@@ -119,18 +119,18 @@ module Less
             scope.path_to_asset(asset)
           end
         end
-        
+
         def context_asset_data_uri(path)
-          
+
         end
-        
+
         def unquote(str)
           s = str.to_s.strip
           s =~ /^['"](.*?)['"]$/ ? $1 : s
         end
-        
+
       end
-      
+
     end
   end
 end
